aae995a155f1586cbeb555de9b3c113c2e8e76b1
Adrien <adrien.laigle@glimps.re>
2021-11-09 17:40:54 +0100

    update improve config

--- src/app/container/container.component.ts
+++ src/app/container/container.component.ts
@@ -1,16 +1,17 @@
-//Importation
-import { Component, OnInit, ViewChild, ComponentFactoryResolver,Type, ViewContainerRef } from '@angular/core';
+// Importation
+import { Component, OnInit, ViewChild, ComponentFactoryResolver, Type, ViewContainerRef } from '@angular/core';
 import { HttpClient, HttpHeaders, HttpClientModule } from '@angular/common/http';
 import { ListComponent } from '../list/list.component';
 import { Directive } from '@angular/core';
 
-@Directive({ selector: '[phvisDisplayCustomers]' })
-  export class DisplayCustomersDirective {
-    constructor() {
-      var d1 = document.getElementById('image_list');
-      d1.insertAdjacentHTML('afterend', 'image_list');
-    }
+const newLocal = '[phvisDisplayCustomers]';
+@Directive({ selector: newLocal })
+export class DisplayCustomersDirective {
+  constructor() {
+    const d1 = document.getElementById('image_list');
+    d1.insertAdjacentHTML('afterend', 'image_list');
   }
+}
 
 @Component({
   selector: 'app-container',
@@ -19,133 +20,141 @@ import { Directive } from '@angular/core';
 })
 
 export class ContainerComponent {
-  @ViewChild('container', {read: ViewContainerRef}) container: ViewContainerRef;
+  @ViewChild('container', { read: ViewContainerRef }) container: ViewContainerRef;
   components = [];
   draggableComponentClass = ListComponent;
   constructor(private componentFactoryResolver: ComponentFactoryResolver, private httpClient: HttpClient) {
   }
 
-  //Fonction qui execute la requête et qui affiche les résultats
-  getImage(componentClass: Type<any>){
-    const app = document.getElementById("results");
-    app.innerText = " ";
-    var user = ((document.getElementById("form_input") as HTMLInputElement).value);
-    if(user.length<1){
+  // Fonction qui execute la requête et qui affiche les résultats
+  getImage(componentClass: Type<any>): void {
+    const app = document.getElementById('results');
+    app.innerText = ' ';
+    const user = ((document.getElementById('form_input') as HTMLInputElement).value);
+    if (user.length < 1) {
       alert('Attention, vous devez saisir un nom d\'utilisateur !!');
-    }else{
-    function removeAllChildNodes(parent) {
-      while (parent.firstChild) {
+    } else {
+      function removeAllChildNodes(parent): void {
+        while (parent.firstChild) {
           parent.removeChild(parent.firstChild);
-      }
-  }
- 
-  const container = document.querySelector('#image_list');
-  removeAllChildNodes(container);
-    this.httpClient
-      .get<any[]>('http://hub.docker.com/v2/repositories/'+user)
-      .subscribe(data => {
-      if(data['count']!=0 ){
-        
-        for (let index = 0; index < data['results'].length; index++) {
-          //Récupération de la date
-          var date = data['results'][index]['last_updated'];
-          if(date == null){
-            date = "Aucune date";
-          }else{
-            date = date.slice(0, 10);
-          }
-          //Ajout des components en fonction du nombre de résultats
-          /*const componentFactory = this.componentFactoryResolver.resolveComponentFactory(componentClass);
-          const component = this.container.createComponent(componentFactory);
-          this.components.push(component);*/ 
-          const app = document.getElementById("image_list");
-          const p = document.createElement("ul");
-          p.innerHTML='<li id="ligne" class="list-group-item" style="margin-top: 1em; background-color: #F5F3F3;"><div><h6>User : '+data['results'][index]['user']+'</h6><h6>Name : '+data['results'][index]['name']+'</h6><h6>Release : ' + date+ '</h6><h6>Description : '+data['results'][index]['description']+'</h6><h6>Modification : '+data['results'][index]['pull_count']+'</h6><img src="../assets/logo.png" style="width:10em;"/></div></li>';
-          app?.appendChild(p);
-
-      }
-        }else{
-          //Si aucun résultat pour le nom rentré par l'utilisateur
-          const app = document.getElementById("image_list");
-          const p = document.createElement("h4");
-          p.innerText = "Aucune image à afficher pour cet utilisateur";
-          app?.appendChild(p);
         }
-      },
-      error => {
-        //Si une erreur se produit lors de la requête
-        alert('Oups, il semblerait que cet utilisateur n\'existe pas...');
       }
-      );
+
+      const container = document.querySelector('#image_list');
+      removeAllChildNodes(container);
+      this.httpClient
+        .get<any[]>('/api/v2/repositories/' + user)
+        .subscribe((data: any) => {
+          const list = document.getElementById('image_list');
+          if (data.count !== 0) {
+
+            // tslint:disable-next-line: prefer-for-of
+            for (let index = 0; index < data.results.length; index++) {
+              // Récupération de la date
+              let date = data.results[index].last_updated;
+              if (date == null) {
+                date = 'Aucune date';
+              } else {
+                date = date.slice(0, 10);
+              }
+              // Ajout des components en fonction du nombre de résultats
+              /*const componentFactory = this.componentFactoryResolver.resolveComponentFactory(componentClass);
+              const component = this.container.createComponent(componentFactory);
+              this.components.push(component);*/
+              const p = document.createElement('ul');
+              p.innerHTML = `<li id="ligne" class="list-group-item" style="margin-top: 1em; background-color: #F5F3F3;"><div><h6>User : ` +
+              data.results[index].user + '</h6><h6>Name : ' + data.results[index].name + '</h6><h6>Release : ' + date + '</h6><h6>Description : ' + data.results[index].description + '</h6><h6>Modification : ' + data.results[index].pull_count + '</h6><img src="../assets/logo.png" style="width:10em;"/></div></li>';
+              list?.appendChild(p);
+
+            }
+          } else {
+            // Si aucun résultat pour le nom rentré par l'utilisateur
+            const p = document.createElement('h4');
+            p.innerText = 'Aucune image à afficher pour cet utilisateur';
+            list?.appendChild(p);
+          }
+        },
+          error => {
+            // Si une erreur se produit lors de la requête
+            alert('Oups, il semblerait que cet utilisateur n\'existe pas...');
+          }
+        );
+    }
   }
-}
 
 
 
-  //Fonction qui execute la requête et qui affiche les résultats en fonction de la date d'aujourd'hui
-  getImage_date(componentClass: Type<any>){
+  // Fonction qui execute la requête et qui affiche les résultats en fonction de la date d'aujourd'hui
+  getImage_date(componentClass: Type<any>): void {
 
-  //Efface les éléments 'enfants' du parent
+    // Efface les éléments 'enfants' du parent
 
-    function removeAllChildNodes(parent) {
+    function removeAllChildNodes(parent): void {
       while (parent.firstChild) {
-          parent.removeChild(parent.firstChild);
+        parent.removeChild(parent.firstChild);
       }
-  }
+    }
 
-    var user = ((document.getElementById("form_input") as HTMLInputElement).value);
-    if(user.length<1){
+    const user = ((document.getElementById('form_input') as HTMLInputElement).value);
+    if (user.length < 1) {
       alert('Attention, vous devez saisir un nom d\'utilisateur !!');
-    }else{
+    } else {
 
-  const container = document.querySelector('#image_list');
-  removeAllChildNodes(container);
-    this.httpClient
-      .get<any[]>('http://hub.docker.com/v2/repositories/'+user)
-      .subscribe(data => {
-        const date1 = new Date().toISOString().slice(0,10);
-        var tab_results = [];
+      const container = document.querySelector('#image_list');
+      removeAllChildNodes(container);
+      const httpOptions = {
+        headers: new HttpHeaders({
+          'Access-Control-Allow-Origin': '*'
+        })
+      };
+      this.httpClient
+        .get<any[]>('/api/v2/repositories/' + user, httpOptions)
+        .subscribe((data: any) => {
+          const date1 = new Date().toISOString().slice(0, 10);
+          const tabResults = [];
 
-      if(data['count']!=0 ){
-        for (let index = 0; index < data['results'].length; index++) {
-          var date = data['results'][index]['last_updated'];
-          if(date == null){
-            date = "Aucune date";
-          }else{
-            date = date.slice(0, 10);
+          if (data.count !== 0) {
+            for (let index = 0; index < data.results.length; index++) {
+              let date = data.results[index].last_updated;
+              if (date == null) {
+                date = 'Aucune date';
+              } else {
+                date = date.slice(0, 10);
+              }
+              if (date1 === date) {
+                console.log(data.results[index]);
+                /*const componentFactory = this.componentFactoryResolver.resolveComponentFactory(componentClass);
+                const component = this.container.createComponent(componentFactory);
+                this.components.push(component);*/
+                const app = document.getElementById('image_list');
+                const p = document.createElement('ul');
+                p.innerHTML =
+                  '<li id="ligne" class="list-group-item" style="margin-top: 1em; background-color: #F5F3F3;"><div><h6>User : ' +
+                  data.results[index].user + '</h6><h6>Name : ' + data.results[index].name + '</h6><h6>Release : ' + date + '</h6><h6>Description : ' + data.results[index].description + '</h6><h6>Modification : ' + data.results[index].pull_count + '</h6></div><img src="../assets/logo.png" style="width:10em;"/></li>';
+                app?.appendChild(p);
+                tabResults.push(index);
+              }
+            }
+
+            // Si aucun résultat pour la date en question (stockés dans le tableau tab_results)
+            if (tabResults.length < 1) {
+              const app = document.getElementById('results');
+              app.innerText = 'Aucune modification récente...';
+            }
+
+          } else {
+            const app = document.getElementById('image_list');
+            const p = document.createElement('h4');
+            p.innerText = 'Aucune image à afficher pour cet utilisateur';
+            app?.appendChild(p);
           }
-          if(date1 == date ){ 
-          console.log(data['results'][index]);
-          /*const componentFactory = this.componentFactoryResolver.resolveComponentFactory(componentClass);
-          const component = this.container.createComponent(componentFactory);
-          this.components.push(component);*/ 
-          const app = document.getElementById("image_list");
-          const p = document.createElement("ul");
-          p.innerHTML='<li id="ligne" class="list-group-item" style="margin-top: 1em; background-color: #F5F3F3;"><div><h6>User : '+data['results'][index]['user']+'</h6><h6>Name : '+data['results'][index]['name']+'</h6><h6>Release : ' + date+ '</h6><h6>Description : '+data['results'][index]['description']+'</h6><h6>Modification : '+data['results'][index]['pull_count']+'</h6></div><img src="../assets/logo.png" style="width:10em;"/></li>';
-          app?.appendChild(p);
-          tab_results.push(index);
+        },
+          error => {
+            alert('Oups, il semblerait que cet utilisateur n\'existe pas...');
           }
-      }
-
-      //Si aucun résultat pour la date en question (stockés dans le tableau tab_results)
-      if(tab_results.length<1){
-        const app = document.getElementById("results");
-        app.innerText = "Aucune modification récente...";
-      }
-
-        }else{
-          const app = document.getElementById("image_list");
-          const p = document.createElement("h4");
-          p.innerText = "Aucune image à afficher pour cet utilisateur";
-          app?.appendChild(p);
-        }
-      },
-      error => {
-        alert('Oups, il semblerait que cet utilisateur n\'existe pas...');
-      }
-      );
+        );
+    }
   }
-}
 
 
 }
